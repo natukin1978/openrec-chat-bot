4ee14d2fadeac92572db8b71162e05fd1ed40d22
natukin1978 <natukin1978@hotmail.com>
2025-07-09 20:18:34 +0900

    Restarting is no longer necessary after changing OneComme CSV.

--- openrec_chat_bot.py
+++ openrec_chat_bot.py
@@ -29,7 +29,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 g.map_is_first_on_stream = {}
-g.one_comme_users = OneCommeUsers.read_one_comme_users()
 g.set_exclude_id = read_text_set("exclude_id.txt")
 # g.set_needs_response = set()
 g.websocket_openrec_live = None
