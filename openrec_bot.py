7e2441f0f6ce99b90a6163ab9d293ea35537a208
natukin1978 <natukin1978@hotmail.com>
2025-09-02 01:10:32 +0900

    Refactoring

--- openrec_bot.py
+++ openrec_bot.py
@@ -6,7 +6,7 @@ import aiohttp
 import global_value as g
 from fuyuka_helper import Fuyuka
 from one_comme_users import OneCommeUsers
-from openrec_message_helper import create_message_json
+from openrec_message_helper import create_message_json_from_ws
 from random_helper import is_hit_by_message_json
 
 logger = logging.getLogger(__name__)
@@ -30,17 +30,11 @@ class OpenrecBot:
             # 無視するID
             return
 
-        json_data = create_message_json()
-        json_data["id"] = id
-        json_data["displayName"] = json_ws["user_name"]
-        if "message" in json_ws:
-            json_data["content"] = json_ws["message"]
-        else:
-            json_data["content"] = ""
-
-        OneCommeUsers.update_message_json(json_data)
-        answerLevel = g.config["fuyukaApi"]["answerLevel"]
-        needs_response = is_hit_by_message_json(answerLevel, json_data)
+        json_data = create_message_json_from_ws(json_ws)
+        answer_level = g.config["fuyukaApi"]["answerLevel"]
+        answer_length = g.config["fuyukaApi"]["answerLength"]["default"]
+        needs_response = is_hit_by_message_json(answer_level, json_data)
+        OneCommeUsers.update_additional_requests(json_data, answer_length)
         await Fuyuka.send_message_by_json_with_buf(json_data, needs_response)
 
     async def get_movie(self):
--- openrec_message_helper.py
+++ openrec_message_helper.py
@@ -1,18 +1,33 @@
 import datetime
 
+from one_comme_users import OneCommeUsers
 
-def create_message_json() -> dict[str, any]:
+
+def create_message_json(
+    id: str, display_name: str, is_first: bool, content: str
+) -> dict[str, any]:
     localtime = datetime.datetime.now()
     localtime_iso_8601 = localtime.isoformat()
     json_data = {
         "dateTime": localtime_iso_8601,
-        "id": None,
-        "displayName": None,
+        "id": id,
+        "displayName": display_name,
         "nickname": None,
-        "content": None,
-        "isFirst": False,
+        "content": content,
+        "isFirst": is_first,
         "isFirstOnStream": None,
         "noisy": False,
         "additionalRequests": None,
     }
+    OneCommeUsers.update_message_json(json_data)
     return json_data
+
+
+def create_message_json_from_ws(json_ws: dict[str, any]) -> dict[str, any]:
+    id = str(json_ws["user_id"])
+    display_name = json_ws["user_name"]
+    is_first = False
+    content = ""
+    if "message" in json_ws:
+        content = json_ws["message"]
+    return create_message_json(id, display_name, is_first, content)
