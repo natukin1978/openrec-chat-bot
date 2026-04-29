e9de2890dcd8e989b38e211f2305830b3cfea9c0
natukin1978 <natukin1978@hotmail.com>
2026-04-29 23:20:43 +0900

    A timeout function has been added to the questions.

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ import sys
 
 import global_value as g
 from config_helper import read_config
+from input_helper import input_with_timeout
 from logging_setup import setup_app_logging
 
 g.app_name = "openrec_chat_bot"
 g.base_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+res = input_with_timeout("前回の続きですか？(y/n) [10秒以内に未入力なら 'n']: ", timeout=10)
+is_continue = (res == "y")
+
 g.config = read_config()
 
 # ロガーの設定
@@ -21,9 +26,6 @@ from openrec_bot import OpenrecBot
 from openrec_helper import OpenrecHelper
 from text_helper import read_text, read_text_set
 from websocket_helper import websocket_listen_forever
-
-print("前回の続きですか？(y/n) ", end="")
-is_continue = input() == "y"
 
 g.ADDITIONAL_REQUESTS_PROMPT = read_text("prompts/additional_requests_prompt.txt")
 
